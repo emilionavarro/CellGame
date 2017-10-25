bbd41ddab0d938dbef55f873f4926ee56c8f5413
Emilio Navarro Cruz <emilio.navarro@gimmal.com>
2017-10-25 15:28:06 -0500

    Added some generic AI for 2 colonies

--- cells/group2.js
+++ cells/group2.js
@@ -1,9 +1,10 @@
 const Cell = require('../classes/Cell');
 const Genes = require('./../enums/Genes');
+const Helpers = require('./../classes/Helpers');
 
 class Group2 extends Cell {
     constructor (lifeSpan, maxOffspring, direction, position, mutate) {
-        super(lifeSpan, maxOffspring, direction, position, "Group 2", mutate);
+        super(lifeSpan, maxOffspring, direction, position, "Group2", mutate);
         this.SetCharacter();
     }
 
@@ -19,12 +20,63 @@ class Group2 extends Cell {
         this.mutation = this.ExchangeMutation;
     }
 
-    CreateChild(position) {
-        return new Group2(this.attributes[Genes.LifeSpan],
-            this.attributes[Genes.MaxOffspring],
-            { x: this.attributes[Genes.MoveX], y: this.attributes[Genes.MoveY] },
-            position,
-            true);
+    MoveCell (board) {
+        var moveLocation = void 0;
+        var quantity = Helpers.GetRandomInRange(0, 1);
+        if (quantity === 0)
+            quantity--;
+
+        var direction = false; // start with horizontal
+        var maxX = this.attributes[Genes.MoveX];
+        var maxY = this.attributes[Genes.MoveY];
+        var moveable = void 0;
+        var canMoveX = true;
+        var canMoveY = true;
+        while ((maxX !== 0 || maxY !== 0) && (canMoveX || canMoveY)) {
+           moveable = this.CanMove(direction, quantity, board);
+
+            if(!direction){
+                if (moveable && maxX !== 0) {
+                    // Remove cell from old location
+                    board._board[this.position.x][this.position.y].Reset(0);
+                    moveLocation = {x: this.position.x + quantity, y: this.position.y};
+                    this.position.x = this.position.x + quantity;
+                    board.MoveCell(this, moveLocation);
+
+                    // dec movement on x
+                    maxX--;
+
+                    canMoveX = true;
+                } else {
+                    direction = !direction;
+                    quantity = Helpers.GetRandomInRange(0, 1);
+                    if (quantity === 0)
+                        quantity--;
+
+                    canMoveX = false;
+                }
+            } else {
+                if (moveable && maxY !== 0) {
+                    // Remove cell from old location
+                    board._board[this.position.x][this.position.y].Reset(0);
+                    moveLocation = {x: this.position.x, y: this.position.y + quantity};
+                    this.position.y = this.position.y + quantity;
+                    board.MoveCell(this, moveLocation);
+
+                    // dec movemenet on y
+                    maxY--;
+
+                    canMoveY = true;
+                } else {
+                    direction = !direction;
+                    quantity = Helpers.GetRandomInRange(0, 1);
+                    if (quantity === 0)
+                        quantity--;
+
+                    canMoveY = false;
+                }
+            }
+        }
     }
 }
 
--- cells/testCell.js
+++ cells/testCell.js
@@ -1,18 +1,127 @@
 const Cell = require('../classes/Cell');
 const Genes = require('./../enums/Genes');
+const Helpers = require('./../classes/Helpers');
 
 class TestCell extends Cell {
     constructor (lifeSpan, maxOffspring, direction, position, mutate) {
-        super(lifeSpan, maxOffspring, direction, position, "TestCell", mutate);
+        super(lifeSpan, maxOffspring, direction, position, "LEO", mutate);
         this.SetCharacter();
     }
 
     SetCharacter() {
-        this.character = "\x1b[1m" + "X" + "\x1b[0m";
+        this.character = "\x1b[1m" + "♫" + "\x1b[0m";
     }
 
     GetType() {
-        return "TestCell";
+        return "LEO";
+    }
+
+    MoveSlow(board) {
+        var moveLocation = void 0;
+        var quantity = -1;
+
+        var direction = false; // start with horizontal
+        var maxX = this.attributes[Genes.MoveX];
+        var maxY = this.attributes[Genes.MoveY];
+
+        var nomoves = 0;
+        var moved = false;
+
+        while(maxX !== 0 || maxY !== 0) {
+            moved = false;
+            if (maxX > 0 && this.CanMove(false, quantity, board)) {
+
+                board._board[this.position.x][this.position.y].Reset(0);
+                moveLocation = {x: this.position.x + quantity, y: this.position.y};
+                this.position.x = this.position.x + quantity;
+                board.MoveCell(this, moveLocation);
+                maxX--;
+                moved = true;
+            }
+
+            if (maxY > 0 && this.CanMove(true, quantity, board)) {
+    
+                board._board[this.position.x][this.position.y].Reset(0);
+                moveLocation = {x: this.position.x, y: this.position.y + quantity};
+                this.position.y = this.position.y + quantity;
+                board.MoveCell(this, moveLocation);
+                maxY--;
+                moved = true;
+                
+            }
+
+            quantity = 1;
+
+            if (maxX > 0 && this.CanMove(false, quantity, board)) {
+
+                board._board[this.position.x][this.position.y].Reset(0);
+                moveLocation = {x: this.position.x + quantity, y: this.position.y};
+                this.position.x = this.position.x + quantity;
+                board.MoveCell(this, moveLocation);
+                maxX--;
+                moved = true;
+
+            }
+
+            if (maxY > 0 && this.CanMove(true, quantity, board)) {
+    
+                board._board[this.position.x][this.position.y].Reset(0);
+                moveLocation = {x: this.position.x, y: this.position.y + quantity};
+                this.position.y = this.position.y + quantity;
+                board.MoveCell(this, moveLocation);
+                maxY--;
+                moved = true;
+
+            }
+
+            if (!moved) {
+                nomoves++;
+            }
+
+            if (nomoves === 5) {
+                break;
+            }
+        }
+    }
+    MoveCell (board) {
+
+        this.MoveSlow(board);
+        return;
+
+        var moveLocation = void 0;
+        var quantity = Helpers.GetRandomInRange(0, 1);
+        if (quantity === 0)
+            quantity--;
+
+        var direction = false; // start with horizontal
+        var maxX = this.attributes[Genes.MoveX];
+        while ((maxX !== 0) && this.CanMove(direction, quantity, board)) {
+            // Remove cell from old location
+            board._board[this.position.x][this.position.y].Reset(0);
+            moveLocation = {x: this.position.x + quantity, y: this.position.y};
+            this.position.x = this.position.x + quantity;
+            board.MoveCell(this, moveLocation);
+
+            // dec movement on x
+            maxX--;
+        }
+
+        direction = true; // now do vertical
+        quantity = Helpers.GetRandomInRange(0, 1);
+        if (quantity === 0)
+            quantity--;
+
+        var maxY = this.attributes[Genes.MoveY];
+        while ((maxY !== 0) && this.CanMove(direction, quantity, board)) {
+            // Remove cell from old location
+            board._board[this.position.x][this.position.y].Reset(0);
+            moveLocation = {x: this.position.x, y: this.position.y + quantity};
+            this.position.y = this.position.y + quantity;
+            board.MoveCell(this, moveLocation);
+
+            // dec movemenet on y
+            maxY--;
+        }
     }
 }
 
